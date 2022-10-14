f1833125b89f85e7d7dda8de4e8ac1bf9026b6bc
Toms1999 <2311929319@qq.com>
2022-10-15 00:39:27 +0800

    error -> warn

--- bin/e_auto.sh
+++ bin/e_auto.sh
@@ -2,7 +2,7 @@
 source ../conf/rocks.conf
 
 #Execute the create external table sql
-echo "source ../result/mysql_to_rocks.sql;" |mysql -h$sr_host -P$sr_master_port -uroot -p$sr_password 2>error.log
+echo "source ../result/mysql_to_rocks.sql;" |mysql -h$sr_host -P$sr_master_port -uroot -p$sr_password 2>warn.log
 
 #Circulation monitoring mysql or conf
 while (( 1 == 1 ))
@@ -12,7 +12,7 @@ do
 sleep 30
 
 #get new create table sql
-sh ./e_mysql_to_rocks.sh ../result/new_mysql_to_rocks.sql 2>error.log
+sh ./e_mysql_to_rocks.sh ../result/new_mysql_to_rocks.sql 2>warn.log
 
 #get a md5 from old create table sql
 old=`md5sum ../result/mysql_to_rocks.sql |awk -F ' ' '{print $1}'`
@@ -26,7 +26,7 @@ new=`md5sum ../result/new_mysql_to_rocks.sql |awk -F ' ' '{print $1}'`
                 do
                        echo "drop table if exists ${table};" |mysql -h$sr_host -P$sr_master_port -uroot -p$sr_password
                 done
-                echo "source ../result/new_mysql_to_rocks.sql;" |mysql -h$sr_host -P$sr_master_port -uroot -p$sr_password 2>error.log
+                echo "source ../result/new_mysql_to_rocks.sql;" |mysql -h$sr_host -P$sr_master_port -uroot -p$sr_password 2>warn.log
 #delete old create table
                 rm -rf ../result/mysql_to_rocks.sql
 #alter new table sql name 
